93bdd56a423ebee80fad3258d3a8eb0199fd1825
Jomet-Franklin <jometfranklin28@proton.me>
2026-05-18 12:35:22 +0530

    Update tf_lite_symptom_pred.dart

--- lib/features/journal/domain/tf_lite_symptom_pred.dart
+++ lib/features/journal/domain/tf_lite_symptom_pred.dart
@@ -1,4 +1,4 @@
-import 'package:pinkrain/features/journal/data/symptom_prediction.dart';
+import '../data/symptom_prediction.dart';
 
 Future<List<SymptomPrediction>> symptomPrediction(String text) async {
   return [];
